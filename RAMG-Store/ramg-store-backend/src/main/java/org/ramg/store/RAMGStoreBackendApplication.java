c4d1f5f4283b40e8e804d566195825642c4d8d33
Rajendra <rajendraks527@gmail.com>
2024-03-03 17:08:07 +0530

    spring security and ModelMapper

--- RAMG-Store/ramg-store-backend/src/main/java/org/ramg/store/RAMGStoreBackendApplication.java
+++ RAMG-Store/ramg-store-backend/src/main/java/org/ramg/store/RAMGStoreBackendApplication.java
@@ -1,11 +1,17 @@
 package org.ramg.store;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RAMGStoreBackendApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RAMGStoreBackendApplication.class, args);
 	}
--- RAMG-Store/ramg-store-backend/src/main/java/org/ramg/store/service/impl/UserServiceImpl.java
+++ RAMG-Store/ramg-store-backend/src/main/java/org/ramg/store/service/impl/UserServiceImpl.java
@@ -1,10 +1,10 @@
 package org.ramg.store.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.ramg.store.constants.ConstantsEnum;
 import org.ramg.store.dto.UserDto;
 import org.ramg.store.entity.User;
 import org.ramg.store.exception.ResourceNotFoundException;
-import org.ramg.store.mapper.UserMapper;
 import org.ramg.store.repository.UserRepository;
 import org.ramg.store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +16,16 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public UserDto createUser(UserDto userDto) {
-        User user = UserMapper.mapToUser(userDto);
+//        User user = UserMapper.mapToUser(userDto);
+        User user = modelMapper.map(userDto, User.class);
         User savedUser = userRepository.save(user);
-        return UserMapper.mapToUserDto(savedUser);
+//        return UserMapper.mapToUserDto(savedUser);
+        return modelMapper.map(savedUser, UserDto.class);
     }
 
     @Override
@@ -28,13 +33,14 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findById(userId).orElseThrow(
                 ()-> new ResourceNotFoundException(ConstantsEnum.USER_NOT_EXISTS.getValue()+userId)
         );
-        return UserMapper.mapToUserDto(user);
+//        return UserMapper.mapToUserDto(user);
+        return modelMapper.map(user, UserDto.class);
     }
 
     @Override
     public List<UserDto> getAllUsers() {
         List<User> users = userRepository.findAll();
-        return users.stream().map(UserMapper::mapToUserDto)
+        return users.stream().map((user) -> modelMapper.map(user, UserDto.class))
                 .collect(Collectors.toList());
     }
 
@@ -57,6 +63,7 @@ public class UserServiceImpl implements UserService {
         user.setContact(updatedUser.getContact());
         user.setEmail(updatedUser.getEmail());
         userRepository.save(user);
-        return UserMapper.mapToUserDto(user);
+//        return UserMapper.mapToUserDto(user);
+        return modelMapper.map(user, UserDto.class);
     }
 }
